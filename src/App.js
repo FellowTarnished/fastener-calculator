99e4e743db5294064eb9f03065ff4d50527d8764
Ken Metz <metzke@umich.edu>
2022-10-31 23:16:29 -0700

    display outstanding  input fields

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./style/App.css";
 import CalcCapacity from "./Calc";
 import React, { useState, useContext, useEffect } from "react";
 import { CapacityContext } from "./CapacityContext";
+import hex from "./style/hex.png";
+import csunk from "./style/csunk.png";
+import ucut from "./style/ucut.png";
 
 function App() {
   const [capacity, setCapacity] = useContext(CapacityContext);
@@ -34,14 +37,14 @@ function App() {
       </header>
       <div className="body">
         <form>
-          <label htmlFor="fastDia"> Fastener Nominal Diameter</label>
+          <div>FASTENER INFO</div>
+          <label htmlFor="fastDia"> Nominal Diameter</label>
           <select
             name="fastDia"
             id="fastDia"
             required
             onChange={(e) => updateProperties(e)}
           >
-            {" "}
             <option hidden disabled selected value>
               -- select an option --
             </option>
@@ -64,19 +67,42 @@ function App() {
               <option value="3/8-16">3/8-16</option>
             </optgroup>
           </select>
-          <div className="type">
-            <button type="button">HEX HEAD</button>
-            <button type="button">COUNTERSUNK</button>
-            <button type="button">UNDERCUT</button>
+          <div>Head Type</div>
+          <div className="headType">
+            <label htmlFor="hexHead">
+              <input
+                type="radio"
+                id="hexHead"
+                name="headType"
+                onChange={(e) => updateProperties(e)}
+              ></input>{" "}
+              <img alt="hex head" src={hex}></img>
+            </label>
+            <label htmlFor="countersunk">
+              <input
+                type="radio"
+                id="countersunk"
+                name="headType"
+                onChange={(e) => updateProperties(e)}
+              ></input>{" "}
+              <img alt="countersunk" src={csunk}></img>
+            </label>
+            <label htmlFor="undercut">
+              <input
+                type="radio"
+                id="undercut"
+                name="headType"
+                onChange={(e) => updateProperties(e)}
+              ></input>{" "}
+              <img alt="undercut" src={ucut}></img>
+            </label>
           </div>
-
-          <label htmlFor="fastMat"> Fastener Nominal Diameter</label>
+          <label htmlFor="fastMat"> Material</label>
           <select
             name="fastMat"
             id="fastMat"
             onChange={(e) => updateProperties(e)}
           >
-            {" "}
             <option hidden disabled selected value>
               -- select an option --
             </option>
@@ -99,10 +125,108 @@ function App() {
               </option>
               <option value="Alloy A4 Cond A">Alloy A4 Cond A</option>
               <option value="Alloy A5 Cond H">Alloy A5 Cond H</option>
-
               <option value="Alloy A5 Cond HT">Alloy A5 Cond HT</option>
             </optgroup>
           </select>
+          <div></div>
+          <div className="flexBox">
+            <label htmlFor="spacing">Spacing</label>
+            <input
+              type="number"
+              id="spacing"
+              step=".1"
+              onChange={(e) => updateProperties(e)}
+            ></input>{" "}
+            <div>inches</div>
+          </div>
+          <div className="flexBox">
+            <label htmlFor="edgeDist">Edge Distance</label>
+            <input
+              type="number"
+              id="edgeDist"
+              step=".1"
+              onChange={(e) => updateProperties(e)}
+            ></input>{" "}
+            <div>inches</div>
+          </div>
+          <div>Are holes slotted?</div>
+          <input
+            type="radio"
+            id="yes"
+            name="slotted"
+            onChange={(e) => updateProperties(e)}
+          ></input>{" "}
+          <label htmlFor="yes">Yes</label>
+          <input
+            type="radio"
+            id="no"
+            name="slotted"
+            onChange={(e) => updateProperties(e)}
+          ></input>{" "}
+          <label htmlFor="no">No</label>
+          <div>----------------</div>
+          <div>COMPONENT 1 INFO (IN CONTACT WITH FASTENER HEAD)</div>
+          <label htmlFor="fastMat">Material</label>
+          <select
+            name="comp1Mat"
+            id="comp1Mat"
+            onChange={(e) => updateProperties(e)}
+          >
+            <option hidden disabled selected value>
+              -- select an option --
+            </option>
+
+            <optgroup label="ALUMINUM">
+              <option value="6063-T6">6063-T6</option>
+              <option value="6063-T5">6063-T5 </option>
+              <option value="6061-T6 EXT">6061-T6 EXTRUSION</option>
+              <option value="6061-T6 PL">6061-T6 PLATE</option>
+              <option value="6005A-T61">6005A-T61</option>
+              <option value="5005-H34">5005-H34</option>
+              <option value="3003-H14">3003-H14</option>
+            </optgroup>
+          </select>
+          <div className="flexBox">
+            <label htmlFor="comp1Thick">Thickness</label>
+            <input
+              type="number"
+              id="comp1Thick"
+              step=".1"
+              onChange={(e) => updateProperties(e)}
+            ></input>{" "}
+            <div>inches</div>
+          </div>
+          <div>----------------</div>
+          <div>COMPONENT 2 INFO (NOT IN CONTACT WITH FASTENER HEAD)</div>
+          <label htmlFor="fastMat">Material</label>
+          <select
+            name="comp2Mat"
+            id="comp2Mat"
+            onChange={(e) => updateProperties(e)}
+          >
+            <option hidden disabled selected value>
+              -- select an option --
+            </option>
+            <optgroup label="ALUMINUM">
+              <option value="6063-T6">6063-T6</option>
+              <option value="6063-T5">6063-T5 </option>
+              <option value="6061-T6 EXT">6061-T6 EXTRUSION</option>
+              <option value="6061-T6 PL">6061-T6 PLATE</option>
+              <option value="6005A-T61">6005A-T61</option>
+              <option value="5005-H34">5005-H34</option>
+              <option value="3003-H14">3003-H14</option>
+            </optgroup>
+          </select>
+          <div className="flexBox">
+            <label htmlFor="comp1Thick">Thickness</label>
+            <input
+              type="number"
+              id="comp2Thick"
+              step=".1"
+              onChange={(e) => updateProperties(e)}
+            ></input>{" "}
+            <div>inches</div>
+          </div>
           <button
             type="button"
             onClick={(e) => {
